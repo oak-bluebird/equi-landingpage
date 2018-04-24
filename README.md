7f8943c65b12510c376223db00e74ece84330a56
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2018-04-24 16:19:11 +0300

    start

---
+++ README.md
@@ -0,0 +1,7 @@
+# WaveLess Dreamkiller - basic template
+
+## Version 3.0.0 ##
+* Updated jquery
+* Updated slick.js
+* Updated fancybox.js
+* Grid changed from flexgrid to bootstrap
---
+++ js/main.js
@@ -0,0 +1,221 @@
+// Global parameters
+window.params = {
+    isMobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+    isIOS: /iPhone|iPad|iPod/i.test(navigator.userAgent)
+};
+
+
+/**
+     *
+     * Check if element exist on page
+     *
+     * @param el {string} jQuery object (#popup)
+     *
+     * @return {bool}
+     *
+*/
+function exist(el){
+    if ( $(el).length > 0 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+jQuery(document).ready(function($) {
+
+    $(".header").headroom();
+    
+    /*---------------------------
+                                  ADD CLASS ON SCROLL
+    ---------------------------*/
+    $(function() { 
+        var $document = $(document),
+            $element = $('.toggle-menu'),
+            $element2 = $('header'),
+            className = 'hasScrolled';
+
+        $document.scroll(function() {
+            $element.toggleClass(className, $document.scrollTop() >= 1);
+            $element2.toggleClass(className, $document.scrollTop() >= 1);
+        });
+    });
+
+
+    /*---------------------------
+                                  File input logic
+    ---------------------------*/
+    $('input[type=file]').each(function(index, el) {
+        $(this).on('change', function(event) {
+            event.preventDefault();
+            var placeholder = $(this).siblings('.placeholder');
+        
+            if ( this.files.length > 0 ) {
+                if ( this.files[0].size < 5000000 ) {
+                    var filename = $(this).val().split('/').pop().split('\\').pop();
+                    if ( filename == '' ) {
+                        filename = placeholder.attr('data-label');
+                    }
+                    placeholder.text(filename);
+                } else {
+                    alert('Maximum file size is 5Mb');
+                }    
+            } else {
+                placeholder.text( placeholder.attr('data-label') );
+            }
+            
+        });
+    });
+    
+    /*---------------------------
+                                PAGE ANCHORS
+    ---------------------------*/
+    $('.page-menu a, .anchor').click(function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 50
+        }, 800);
+        return false;
+    });
+
+    /*---------------------------
+                                  MENU TOGGLE
+    ---------------------------*/
+    $('.js-toggle-menu').on('click', function(event) {
+        event.preventDefault();
+        $(this).toggleClass('is-active');
+        $(this).siblings('header').toggleClass('open');
+    });
+
+
+
+
+    var time = new Date( Date.UTC(2018, 4, 15, 0, 0, 0) );
+
+    $('.countdown').countdown({
+        until: time,
+        padZeroes: true
+    }); 
+
+
+
+    $('.js-close-telegram-popup').on('click', function(event) {
+        event.preventDefault();
+        
+
+        $('.telegram-notification').fadeOut('500');
+    });
+
+
+
+    /*---------------------------
+                                  Fancybox
+    ---------------------------*/
+    $('.fancybox').fancybox({
+        
+    });
+
+
+    /**
+     *
+     * Open popup
+     *
+     * @param popup {String} jQuery object (#popup)
+     *
+     * @return n/a
+     *
+    */
+    function openPopup(popup){
+        $.fancybox.open([
+            {
+                src  : popup,
+                type: 'inline',
+                opts : {}
+            }
+        ], {
+            loop : false
+        });
+    }
+
+
+
+    /*---------------------------
+                                  Form submit
+    ---------------------------*/
+    $('.ajax-form').on('submit', function(event) {
+        event.preventDefault();
+        var data = new FormData(this);
+        $(this).find('button').prop('disabled', true);
+        $.ajax({
+            url: theme.url + '/forms.php',
+            type: 'POST',
+            data: data,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(result) {
+                if (result.status == 'ok') {
+                    openPopup('#modal-popup-ok')
+                } else {
+                    openPopup('#modal-popup-error')
+                }
+            },
+            error: function(result) {
+                openPopup('#modal-popup-error');
+            }
+        }).always(function() {
+            $('form').each(function(index, el) {
+                $(this)[0].reset();
+                $(this).find('button').prop('disabled', false);
+            });
+        });
+    });
+
+
+
+    /*---------------------------
+                                  Google map init
+    ---------------------------*/
+    var map;
+    function googleMap_initialize() {
+        var lat = $('#map_canvas').data('lat');
+        var long = $('#map_canvas').data('lng');
+
+        var mapCenterCoord = new google.maps.LatLng(lat, long);
+        var mapMarkerCoord = new google.maps.LatLng(lat, long);
+
+        var styles = [];
+
+        var mapOptions = {
+            center: mapCenterCoord,
+            zoom: 16,
+            //draggable: false,
+            disableDefaultUI: true,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+        var styledMapType=new google.maps.StyledMapType(styles,{name:'Styled'});
+        map.mapTypes.set('Styled',styledMapType);
+        map.setMapTypeId('Styled');
+
+        var markerImage = new google.maps.MarkerImage('images/location.png');
+        var marker = new google.maps.Marker({
+            icon: markerImage,
+            position: mapMarkerCoord, 
+            map: map,
+            title:"Site Title"
+        });
+        
+        $(window).resize(function (){
+            map.setCenter(mapCenterCoord);
+        });
+    }
+
+    if ( exist( '#map_canvas' ) ) {
+        googleMap_initialize();
+    }
+
+}); // end file
