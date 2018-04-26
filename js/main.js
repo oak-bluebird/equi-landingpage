cee24718b2edaa87d5b8d60d82ed8ee4f8c8f6e7
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2018-04-26 16:24:22 +0300

    popup added

--- js/main.js
+++ js/main.js
@@ -147,15 +147,27 @@ jQuery(document).ready(function($) {
 
 
 
-    var wow = new WOW(
-      {
-        boxClass:     'wow',      // animated element css class (default is wow)
-        animateClass: 'animated', // animation css class (default is animated)
-        offset:       200,          // distance to the element when triggering the animation (default is 0)
-        mobile:       true,       // trigger animations on mobile devices (default is true)
-      }
-    );
-    wow.init();
+    
+
+
+
+
+    $('.js-continue').on('click', function(event) {
+        event.preventDefault();
+        
+        $('.site-popup-holder').fadeOut('1000');
+        $('body').removeClass('site-popup-opened');
+
+        var wow = new WOW(
+          {
+            boxClass:     'wow',      // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset:       100,          // distance to the element when triggering the animation (default is 0)
+            mobile:       true,       // trigger animations on mobile devices (default is true)
+          }
+        );
+        wow.init();
+    });
 
 
 
